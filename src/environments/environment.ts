18a452a34ee18fccf50b22a55367cfa8f9646260
Edward P. Legaspi <czetsuya@gmail.com>
2019-08-17 13:17:25 +0800

    Update environment settings.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@
 export const environment = {
     baseUrl: 'http://localhost:4200',
     production: false,
-    apiUrl: 'http://localhost:8280/keycloak-auth-api/rest',
+    apiUrl: 'http://localhost:8080/keycloak-auth-api/rest',
 
     keycloakRealm: 'keycloak-auth',
     keycloakClient: 'auth-client',
